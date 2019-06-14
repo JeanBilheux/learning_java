5edc06b45c97333dd45af686a87ca4e564b2ec11
JeanBilheux <bilheuxjm@ornl.gov>
2019-06-13 21:17:40 -0400

    Added a few basics examples

---
+++ TapeDeck/TapeDeck.java
@@ -0,0 +1,15 @@
+package TapeDeck;
+
+public class TapeDeck {
+	
+	boolean canRecord = false;
+
+	public void playTape() {
+		System.out.println("tape playing");
+	}
+
+	public void recordTap() {
+		System.out.println("tape recording");
+	}
+
+}
